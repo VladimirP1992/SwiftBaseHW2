499286efabc164ce21f4265b02f1f1a19480f5ab
Vladimir <p.vladimir.m@yandex.ru>
2020-12-20 18:00:09 +0300

    Task 2 completed

--- SwiftBaseHW2/main.swift
+++ SwiftBaseHW2/main.swift
@@ -7,11 +7,20 @@
 
 import Foundation
 
+let number = 9
+
 //Task1: even or not even function
 func isEven(number: Int) -> Bool {
     return number % 2 == 0
 }
 
-print(isEven(number: 9))
+print("\(number) is even = \(isEven(number: number))")
+
+//Task2: remainder of division by 3
+func isDivisiblebyThree(number: Int) -> Bool {
+    return number % 3 == 0
+}
+
+print("\(number) is divisible by 3 = \(isDivisiblebyThree(number: number))")
 
 
